7cd0e111978e6dcdf210caa0d43cb7448dd028b1
Your Name <you@example.com>
2023-01-10 18:52:33 +0530

    setup

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        animal = request.form["animal"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(animal),
+            temperature=0.7,
+            max_tokens=2048,
+            top_p=1
+        )
+        print(response)
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_prompt(animal):
+    return f""" genrate the ppt content slide by slide with titles, it has 8 slides,also add refernce links in it, also add some data numbers on the topic {animal} in json type"""
